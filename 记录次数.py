b0d01c2847a3012320c08e6bf1128c3abc6a6216
chaojishuai <3487836@qq.com>
2018-05-22 12:44:23 +0800

    b

--- 记录次数.py
+++
@@ -1,9 +0,0 @@
-def findstr(sub1,sub2):
-    count = 0
-    lenth = len(sub1)
-    if sub2 in sub1:
-        for each in range(lenth-1):
-            if sub1[each] == sub2[0]:
-                if sub1[each + 1] == sub2[1]:
-                    count += 1
-    print(count)
